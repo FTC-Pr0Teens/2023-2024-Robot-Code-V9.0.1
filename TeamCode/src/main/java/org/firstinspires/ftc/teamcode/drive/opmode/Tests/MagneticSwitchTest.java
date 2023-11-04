4dbe53a87a9a2c6752bd332ab1d5956f948d587e
NS678 <nsnew567@gmail.com>
2023-11-04 16:45:38 -0400

    Update MagneticSwitchTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Tests/MagneticSwitchTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Tests/MagneticSwitchTest.java
@@ -8,28 +8,21 @@ import org.firstinspires.ftc.teamcode.subsystems.MultiMotorSubsystem;
 
 @TeleOp
 public class MagneticSwitchTest extends LinearOpMode {
-    // Define variables for our touch sensor and motor
     TouchSensor magnet;
 
     @Override
     public void runOpMode() {
-        // Get the touch sensor and motor from hardwareMap
         magnet = hardwareMap.get(TouchSensor.class, "magnet");
         MultiMotorSubsystem multiMotorSubsystem = new MultiMotorSubsystem(hardwareMap, true, MultiMotorSubsystem.MultiMotorType.dualMotor);
 
-        // Wait for the play button to be pressed
         waitForStart();
 
-        // Loop while the Op Mode is running
         while (opModeIsActive()) {
             double tickNum = multiMotorSubsystem.getPosition();
-            if (magnet.isPressed()) {
-                telemetry.addData("pressed","");
-
+            if (magnet.isPressed() && gamepad1.left_stick_y <0) {
                 multiMotorSubsystem.moveLift(tickNum);
             }
             else {
-                telemetry.addData("not pressed","");
                 multiMotorSubsystem.moveLift(gamepad1.left_stick_y);
             }
 
