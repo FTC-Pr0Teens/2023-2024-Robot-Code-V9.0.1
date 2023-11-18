cabaca2843649e31633bd8b25c4089dd39935fde
pekachoo <jliu6162@gmail.com>
2023-11-18 13:19:37 -0500

    test organization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/MainTeleOp.java
@@ -10,6 +10,9 @@ import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.MecanumSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.MultiMotorSubsystem;
 import org.firstinspires.ftc.teamcode.threadopmode.subsystems.OdometrySubsystem;
+import org.firstinspires.ftc.teamcode.threadopmode.subsystems.IMUSubsystem;
+import org.firstinspires.ftc.teamcode.util.GridAutoCentering;
+import org.firstinspires.ftc.teamcode.util.GyroOdometry;
 
 public class MainTeleOp extends LinearOpMode{
 
@@ -21,25 +24,83 @@ public class MainTeleOp extends LinearOpMode{
 //    private OutputCommand outputCommand;
     private IntakeSubsystem intakeSubsystem;
     private IntakeCommand intakeCommand;
+    private IMUSubsystem imuSubsystem;
+    private GyroOdometry gyroOdometry;
+    private GridAutoCentering gridAutoCentering;
+
     @Override
     public void runOpMode() throws InterruptedException {
 
         multiMotorSubsystem = new MultiMotorSubsystem(hardwareMap, true, MultiMotorSubsystem.MultiMotorType.dualMotor);
         multiMotorCommand = new MultiMotorCommand(multiMotorSubsystem);
 
+        imuSubsystem = new IMUSubsystem(hardwareMap, null);
+
+        gyroOdometry = new GyroOdometry(odometrySubsystem, imuSubsystem, null);
         odometrySubsystem = new OdometrySubsystem(hardwareMap);
 
         mecanumSubsystem = new MecanumSubsystem(hardwareMap);
-        mecanumCommand = new MecanumCommand(mecanumSubsystem, odometrySubsystem, null, null, null, null, null, this, null);
+        mecanumCommand = new MecanumCommand(mecanumSubsystem, odometrySubsystem, null, null, null, null, gyroOdometry, this, null);
 
 //        outputCommand = new OutputCommand(hardwareMap);
 
         intakeSubsystem = new IntakeSubsystem(hardwareMap);
         intakeCommand = new IntakeCommand(intakeSubsystem);
 
+        gridAutoCentering = new GridAutoCentering(mecanumSubsystem, gyroOdometry);
+
         waitForStart();
 
-        mecanumCommand.moveGlobalPartial();
+        if(gamepad1.a){
+            multiMotorCommand.LiftUp(true, 0);
+        }
+        else if(gamepad1.b){
+            multiMotorCommand.LiftUp(true, 3);
+        }
+        else if(gamepad1.y){
+            multiMotorCommand.LiftUp(true, 1);
+        }
+        else if(gamepad1.x){
+            multiMotorCommand.LiftUp(true, 2);
+        }
+        else {
+            multiMotorSubsystem.moveLift(0);
+        }
+
+        mecanumCommand.moveGlobalPartial(true, gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+
+        if(gamepad1.right_trigger > 0.5){
+            intakeCommand.intakeIn();
+        }
+        else if(gamepad1.left_trigger > 0.5){
+            intakeCommand.intakeOut();
+        }
+        else{
+            intakeCommand.stopIntake();
+        }
+
+        if(gamepad1.dpad_left){
+            gridAutoCentering.offsetTargetAngle(-Math.PI/2);
+            gridAutoCentering.process(true);
+        }
+        else if(gamepad1.dpad_right){
+            gridAutoCentering.offsetTargetAngle(Math.PI/2);
+            gridAutoCentering.process(true);
+        }
+        else if(gamepad1.dpad_up){
+            gridAutoCentering.offsetTargetAngle(0);
+            gridAutoCentering.process(true);
+        }
+        else if(gamepad1.dpad_down){
+            gridAutoCentering.offsetTargetAngle(Math.PI);
+            gridAutoCentering.process(true);
+        } else if (gamepad1.right_bumper) {
+            gridAutoCentering.reset();
+        }
+        else{
+            gridAutoCentering.process(false);
+        }
+
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Tests/MecanumTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Tests/MecanumTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.drive.opmode;
+package org.firstinspires.ftc.teamcode.drive.opmode.Tests;
 
 /***
  * 9 September 2023, Dylan Chunyu
