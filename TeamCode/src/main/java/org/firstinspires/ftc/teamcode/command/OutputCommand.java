459da6629f17bd9f45b0c4b9b8b884c065c16b14
DeadPineapple <blockboom557@gmail.com>
2023-12-02 01:38:34 -0500

    im going to sleep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/command/OutputCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/command/OutputCommand.java
@@ -1,4 +1,39 @@
 package org.firstinspires.ftc.teamcode.command;
 
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.subsystems.SingleMotorSubsystem;
+import org.firstinspires.ftc.teamcode.util.Specifications;
+import org.firstinspires.ftc.teamcode.util.TimerList;
+
+import java.util.Timer;
+
 public class OutputCommand {
+
+    //Assuming lift is at the front of robot and intake at back
+    private Servo leftArm;
+    private Servo rightArm;
+    private Servo leftTilt;
+    private Servo rightTilt;
+    private Servo gate;
+    private CRServo outputWheel;
+
+    private TimerList timers = new TimerList();
+
+
+    public OutputCommand(HardwareMap hardwareMap) {
+        leftArm = hardwareMap.get(Servo.class, Specifications.LEFT_OUTPUT_ARM);
+        rightArm = hardwareMap.get(Servo.class, Specifications.RIGHT_OUTPUT_ARM);
+    }
+
+    public void dropPixel(Gamepad gamepad){
+        //i might actualy put this back in teleop ngl
+
+        // check block code for specific timings. It is 200 ms for the gate, 200 ms for the wheel.
+    }
+
+
 }
