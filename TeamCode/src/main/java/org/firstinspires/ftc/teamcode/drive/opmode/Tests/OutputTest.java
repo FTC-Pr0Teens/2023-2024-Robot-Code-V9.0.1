ab95aeb5dfc3a3fa138dfaa6949cde0af8f51175
pekachoo <jliu6162@gmail.com>
2023-12-02 20:49:45 -0500

    Update OutputTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Tests/OutputTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Tests/OutputTest.java
@@ -1,18 +1,21 @@
 package org.firstinspires.ftc.teamcode.drive.opmode.Tests;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.command.OutputCommand;
 
+@Config
 @TeleOp(name="Output Test")
 public class OutputTest extends LinearOpMode {
     private OutputCommand outputCommand;
 
+    public static double position = 0.5;
     @Override
     public void runOpMode() throws InterruptedException {
         outputCommand = new OutputCommand(hardwareMap);
-
+//        outputCommand.initialize();
         waitForStart();
 
         while(opModeIsActive()){
@@ -37,12 +40,16 @@ public class OutputTest extends LinearOpMode {
             else if(gamepad1.right_trigger > 0.5){
                 outputCommand.outputWheelOut();
             }
+            else{
+                outputCommand.outputWheelStop();
+            }
 
             telemetry.addData("gate position", outputCommand.getGatePosition());
             telemetry.addData("left arm position", outputCommand.getLeftArmPosition());
             telemetry.addData("right arm position", outputCommand.getRightArmPosition());
             telemetry.addData("left tilt position", outputCommand.getLeftTiltPosition());
             telemetry.addData("right tilt position", outputCommand.getRightTiltPosition());
+            telemetry.update();
         }
     }
 }
