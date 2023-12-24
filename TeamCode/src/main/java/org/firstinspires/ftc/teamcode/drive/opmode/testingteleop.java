494e3cf54ebd333cbb7d42864a60fea8d350bd71
Muki-Anuwa <143140471+Muki-Anuwa@users.noreply.github.com>
2023-12-24 15:15:09 -0500

    Update testingteleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/testingteleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/testingteleop.java
@@ -11,7 +11,9 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.command.IntakeCommand;
 import org.firstinspires.ftc.teamcode.command.MultiMotorCommand;
+import org.firstinspires.ftc.teamcode.subsystems.DroneShooter;
 import org.firstinspires.ftc.teamcode.subsystems.IMUSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.MecanumSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.MultiMotorSubsystem;
@@ -21,11 +23,17 @@ import org.firstinspires.ftc.teamcode.util.Specifications;
 @TeleOp(name = "Pr0TeensTeleOpV1")
 public class testingteleop extends LinearOpMode {
     private MecanumSubsystem mecanumSubsystem;
+    private MultiMotorSubsystem multiMotorSubsystem;
+    private MultiMotorCommand multiMotorCommand;
+
+    private IntakeCommand intakeCommand;
 
     private IMUSubsystem imuSubsystem;
 
     private DcMotorEx leftSlide;
     private DcMotorEx rightSlide;
+
+    private DroneShooter droneShooter;
 
     private enum RUNNING_STATE { //mini "threads" to run (is actually run in main thread, just controlled simultaneously)
         LOWER_LIFT, RAISE_LIFT
@@ -48,20 +56,24 @@ public class testingteleop extends LinearOpMode {
         imuSubsystem = new IMUSubsystem(hardwareMap);
         mecanumSubsystem = new MecanumSubsystem(hardwareMap); //MAKE SURE THIS IS INSTANTIATED BEFORE ODOMETRY SUBSYSTEM
 
+        multiMotorSubsystem = new MultiMotorSubsystem(hardwareMap, true, MultiMotorSubsystem.MultiMotorType.dualMotor);
+        multiMotorCommand = new MultiMotorCommand(multiMotorSubsystem);
+
+        intakeCommand = new IntakeCommand(hardwareMap);
         waitForStart();
 
         while (opModeIsActive()) {
             if (gamepad1.dpad_down) {
-                leftSlide.setPower(-0.2);
-                rightSlide.setPower(-0.2);
+                multiMotorCommand.LiftUp(true, 1);
             } else if (gamepad1.dpad_up) {
-                leftSlide.setPower(0.2);
-                rightSlide.setPower(0.2);
+                multiMotorCommand.LiftUp(true, 2);
             } else {
-                leftSlide.setPower(0);
-                leftSlide.setPower(0);
+                multiMotorCommand.LiftUp(false, 0);
             }
             mecanumSubsystem.fieldOrientedMove(-gamepad1.left_stick_x, gamepad1.left_stick_y, -gamepad1.right_stick_x, imuSubsystem.getTheta());
+
+            intakeCommand.lowerIntake();
+
 
 
         }
