fe52023f9db4cb65345201e9327162f6fcc58f63
NotMax08 <143467878+NotMax08@users.noreply.github.com>
2024-01-07 16:36:22 -0500

    hi
    ho

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MultiMotorSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MultiMotorSubsystem.java
@@ -140,8 +140,8 @@ public class MultiMotorSubsystem extends Specifications {
             main = hardwareMap.get(DcMotorEx.class, EXTENSION_MOTOR_MAIN);
             aux1 = hardwareMap.get(DcMotor.class, EXTENSION_MOTOR_AUX1);
 
-            main.setDirection(DcMotorSimple.Direction.REVERSE); //originally reverse
-            aux1.setDirection(DcMotorSimple.Direction.FORWARD);
+            main.setDirection(DcMotorSimple.Direction.FORWARD); //originally reverse
+            aux1.setDirection(DcMotorSimple.Direction.REVERSE);
 
             if (reset) {
                 main.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
