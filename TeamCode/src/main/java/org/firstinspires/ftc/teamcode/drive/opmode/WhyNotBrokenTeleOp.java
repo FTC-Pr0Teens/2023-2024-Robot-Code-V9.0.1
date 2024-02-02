9d71b4c6e731c57512bd1bac1b4f7ce299fc95a1
Robotli89 <121524712+Robotli89@users.noreply.github.com>
2024-02-01 19:35:42 -0500

    Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/WhyNotBrokenTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/WhyNotBrokenTeleOp.java
@@ -1,40 +1,22 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
-import com.arcrobotics.ftclib.hardware.motors.Motor;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.teamcode.subsystems.DroneShooter;
 import org.firstinspires.ftc.teamcode.subsystems.NotMecanumSubsystem;
-import org.firstinspires.ftc.teamcode.util.Specifications;
 
 @TeleOp(name = "WhyNotBrokenTeleOP")
 public class WhyNotBrokenTeleOp extends LinearOpMode {
-    private DcMotor leftMotor;
-    private DcMotor rightMotor;
-    private NotMecanumSubsystem NotMecanumSubsystem;
-
-    private DcMotor ArmMotor;
-
-    private DroneShooter DroneShooter;
+    private NotMecanumSubsystem notMecanumSubsystem;
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
+        notMecanumSubsystem = new NotMecanumSubsystem(hardwareMap);
+
         waitForStart();
-        while(opModeIsActive()){
-            NotMecanumSubsystem.move(gamepad1.left_stick_y, gamepad1.right_stick_y);
-
-
+        while(opModeIsActive()) {
+            double leftPower = -gamepad1.left_stick_y; // Inverting to match joystick direction
+            double rightPower = -gamepad1.right_stick_y; // Inverting to match joystick direction
+            notMecanumSubsystem.move(leftPower, rightPower);
         }
     }
-
-
-
-
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/NotMecanumSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/NotMecanumSubsystem.java
@@ -1,49 +1,37 @@
 package org.firstinspires.ftc.teamcode.subsystems;
-import com.arcrobotics.ftclib.hardware.motors.Motor;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.teamcode.subsystems.DroneShooter;
-import org.firstinspires.ftc.teamcode.util.Specifications;
 
 public class NotMecanumSubsystem {
     private DcMotor leftMotor;
     private DcMotor rightMotor;
 
-    private DcMotor ArmMotor;
-
-    private DroneShooter DroneShooter;
-
-
-    public void NotMecanumDrive(HardwareMap hardwareMap){
-
+    public NotMecanumSubsystem(HardwareMap hardwareMap) {
+        // Initialize motors
         leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
-        rightMotor = hardwareMap.get(DcMotorEx.class, "rightMotor");
+        rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
 
+        // Set motor directions
         leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         rightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
+        // Set motor modes
         leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+        // Set zero power behavior
         leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        // Set initial power
         leftMotor.setPower(0);
         rightMotor.setPower(0);
-
-
     }
 
-    public void move(double rightPower, double leftPower){
+    public void move(double leftPower, double rightPower) {
         leftMotor.setPower(leftPower);
         rightMotor.setPower(rightPower);
     }
-
-
 }
